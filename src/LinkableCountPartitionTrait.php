ec4b478becb686214a65b19bbe5682947a14ff00
Pedro Veiga <pedro.veiga@cortex-intelligence.com>
2022-01-10 20:27:59 -0300

    feature(linkable-count): adding linkable count using multiples urls

---
+++ src/LinkableCountPartitionTrait.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace SaintSystems\Nova\LinkableMetrics;
+
+trait LinkableCountPartitionTrait {
+    use LinkableCount;
+
+    /**
+     * Create a new value metric result.
+     *
+     * @param  mixed  $value
+     * @return LinkableCountPartitionResult
+     */
+    public function result(array $value)
+    {
+        $linkablePartitionResult = new LinkableCountPartitionResult($value);
+        if (!empty($this->urls)) {
+            $linkablePartitionResult->urls = $this->urls;
+        }
+        return $linkablePartitionResult;
+    }
+}
